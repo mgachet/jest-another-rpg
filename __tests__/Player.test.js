aee2163b3201d8080edace422c1f5a3d625207d3
Marcelo Gachet <marcelo@growdnd.com>
2022-03-29 14:12:36 -0700

    Added Player constructor

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -2,7 +2,7 @@ const Player = require('../lib/Player');
 const Potion = require('../lib/Potion');
 jest.mock('../lib/Potion');
 
-test('creates a player', ()=>{
+test('creates a player', () => {
     const player = new Player('Test');
 
     expect(player.name).toBe('Test');
@@ -11,4 +11,24 @@ test('creates a player', ()=>{
     expect(player.agility).toEqual(expect.any(Number));
     expect(player.inventory).toEqual(
         expect.arrayContaining([expect.any(Object)]));
-})
+});
+
+test('gets player stats as an object', () => {
+    const player = new Player("Test");
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player("Test");
+
+    expect(player.getInventory()).toEqual(
+        expect.any(Array));
+    player.inventory = [];
+    expect(player.getInventory()).toEqual(false);
+
+});
+
