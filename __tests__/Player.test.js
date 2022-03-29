9a82d8cde571ccafb607f39fbb0baff40eb4a46a
Marcelo Gachet <marcelo@growdnd.com>
2022-03-29 14:45:25 -0700

    Player methods completed

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -32,3 +32,32 @@ test('gets inventory from player or returns false', () => {
 
 });
 
+test('get player\'s health value', () => {
+    const player = new Player("Test");
+
+    expect(player.getHealth()).toEqual(
+        expect.stringContaining(player.health.toString()));
+});
+
+test('checks if player is alive or not', () => {
+    const player = new Player("Test");
+
+    expect(player.isAlive()).toBeTruthy();
+    
+    player.health = 0;
+    expect(player.isAlive()).toBeFalsy();
+});
+
+test("subtracts from player's health", () => {
+    const player = new Player("Test");
+    const oldHealth = player.health;
+  
+    player.reduceHealth(5);
+  
+    expect(player.health).toBe(oldHealth - 5);
+  
+    player.reduceHealth(99999);
+  
+    expect(player.health).toBe(0);
+  });
+
